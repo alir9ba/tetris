bdf0e57e8db712fb0b977bf5c5d2c2c60179313b
Alireza Bagheri <alir9ba@Alirezas-MacBook-Pro.local>
2026-01-19 04:33:16 +0300

    feat: home screen with navigation buttons

---
+++ src/constants/tetrominos.js
@@ -0,0 +1,66 @@
+export const TETROMINOS = {
+  0: { shape: [[0]], color: '0, 0, 0' }, // Empty cell
+  I: {
+    shape: [
+      [0, 'I', 0, 0],
+      [0, 'I', 0, 0],
+      [0, 'I', 0, 0],
+      [0, 'I', 0, 0],
+    ],
+    color: '#80DEEA', // Cyan
+  },
+  J: {
+    shape: [
+      [0, 'J', 0],
+      [0, 'J', 0],
+      ['J', 'J', 0],
+    ],
+    color: '#9FA8DA', // Blue
+  },
+  L: {
+    shape: [
+      [0, 'L', 0],
+      [0, 'L', 0],
+      [0, 'L', 'L'],
+    ],
+    color: '#FFCC80', // Orange
+  },
+  O: {
+    shape: [
+      ['O', 'O'],
+      ['O', 'O'],
+    ],
+    color: '#FFF59D', // Yellow
+  },
+  S: {
+    shape: [
+      [0, 'S', 'S'],
+      ['S', 'S', 0],
+      [0, 0, 0],
+    ],
+    color: '#A5D6A7', // Green
+  },
+  T: {
+    shape: [
+      [0, 0, 0],
+      ['T', 'T', 'T'],
+      [0, 'T', 0],
+    ],
+    color: '#CE93D8', // Purple
+  },
+  Z: {
+    shape: [
+      ['Z', 'Z', 0],
+      [0, 'Z', 'Z'],
+      [0, 0, 0],
+    ],
+    color: '#EF9A9A', // Red
+  },
+};
+
+export const randomTetromino = () => {
+  const tetrominos = 'IJLOSTZ';
+  const randTetromino =
+    tetrominos[Math.floor(Math.random() * tetrominos.length)];
+  return TETROMINOS[randTetromino];
+};
